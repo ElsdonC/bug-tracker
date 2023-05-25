ec733690fb39e737138566dedf27c7ff8e41ad57
ElsdonC <elsdonc21@gmail.com>
2023-05-24 17:38:10 -0700

    implemented registration and login

--- server/index.js
+++ server/index.js
@@ -4,21 +4,48 @@ const cors = require("cors");
 const app = express()
 const PORT = process.env.PORT
 const pool = require('./db')
+const bcrypt = require('bcrypt')
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    const users = await pool.query("SELECT * FROM users WHERE user_id = 1")
+    const users = await pool.query("SELECT * FROM users")
     res.json({ "users": users.rows[0] })
 })
 
-app.post('/user', async (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
+app.post('/register', async (req, res) => {
+    const saltRounds = 10
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+    const plainTextPassword = req.body.password
     const email = req.body.email
-    await pool.query("INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",[username, password, email])
+    bcrypt.hash(plainTextPassword, saltRounds, async function(err, hash) {
+        if (err) {
+            console.log(err)
+            res.json("Something went wrong")
+            return
+        } else {
+            await pool.query("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",[first_name, last_name, email, hash])
+        }
+    });
     res.json("Successfully Created Account")
+})
+
+app.post('/login', async (req, res) => {
+    const email = req.body.email
+    const enteredPassword = req.body.password
+    const user = await pool.query("SELECT * FROM users WHERE email = $1", [email])
+    const hash = user.rows[0].password
+    bcrypt.compare(enteredPassword, hash, function(err, result) {
+        if (err) {
+            console.log(err)
+        } else if (result == true) {
+            res.json("Correct Password")
+        } else {
+            res.json("Wrong Password")
+        }
+    })
 })
 
 app.listen(PORT || process.env.PORT, () => {
